d8a0a26b3221778475e8c08cf25b0ea93dcf419f
Serhii Lysovenko <Lysovenko@users.noreply.github.com>
2020-09-28 16:47:40 +0300

    show prettier (stdio) menu

--- core/stdio/__init__.py
+++ core/stdio/__init__.py
@@ -19,7 +19,7 @@ from cmd import Cmd
 from ..application import APPLICATION as APP, Opener
 from .dialog import (
     print_error, print_information, ask_question, ask_open_filename,
-    ask_save_filename, Dialogs)
+    ask_save_filename, input_dialog, Dialogs)
 
 _WIN_STACK = []
 
@@ -52,11 +52,23 @@ class Xrcmd(Cmd):
         Opener.open_by_name(line)
 
     def do_menu(self, line):
-        path = tuple(line.split("/"))
-        last = dict(_WIN_STACK[-1].menu.get_items(path[:-1], APP.menu))
-        mi = last.get(path[-1])
-        if mi is not None and callable(mi.function):
-            mi.function()
+        if not line:
+            print("\n".join(
+                f"{n+1:3} {'/'.join(p)}" for n, (p, i) in enumerate(
+                    yield_menu(_WIN_STACK[-1].menu, ()))))
+            return
+        try:
+            n = int(line) - 1
+            funcs = [i for p, i in yield_menu(_WIN_STACK[-1].menu, ())]
+            if 0 <= n < len(funcs):
+                if callable(funcs[n]):
+                    funcs[n]()
+                    return
+        except ValueError:
+            p = tuple(line.split("/"))
+            f = dict(yield_menu(_WIN_STACK[-1].menu, ())).get(p)
+            if callable(f):
+                f()
 
 
 def main():
@@ -71,14 +83,12 @@ def show_vi(vi_obj):
     vi_obj.gui_functions["set_choicer"] = lambda *args: None
     Dialogs(vi_obj)
     _WIN_STACK.append(vi_obj)
-    print("\n".join(
-        "/".join(p) for p, i in yield_menu(vi_obj.menu, APP.menu, ())))
 
 
-def yield_menu(menu, other, path: tuple):
-    for n, mi in menu.get_items(path, other):
+def yield_menu(menu, path: tuple):
+    for n, mi in menu.get_items(path, APP.menu):
         if isinstance(mi.function, dict):
-            for i in yield_menu(menu, other, path + (n,)):
+            for i in yield_menu(menu, path + (n,)):
                 yield i
         elif mi.function is not None:
             yield (path + (n,), mi.function)
