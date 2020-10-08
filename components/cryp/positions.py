0a5d94676f4cee18b60f42387d82f5d93b69db3d
Serhii Lysovenko <Lysovenko@users.noreply.github.com>
2020-10-09 02:03:19 +0300

    badly lost day

---
+++ components/cryp/positions.py
@@ -0,0 +1,21 @@
+# XRCEA (C) 2020 Serhii Lysovenko
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or (at
+# your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+"""Spreadsheet with some peaks positions"""
+
+from core.vi.spreadsheet import Spreadsheet
+from core.application import APPLICATION as APP
+from core.vi.value import Tabular, TabCell, Value
+from core.vi import Button
