f33fcab3c80070df8603233627bb1e7e6d97e4c0
Serhii Lysovenko <Lysovenko@users.noreply.github.com>
2021-02-24 00:55:00 +0200

    write Miller indices to table and save them

--- components/cryp/positions.py
+++ components/cryp/positions.py
@@ -23,6 +23,9 @@ from core.vi.value import Tabular, TabCell, Value
 from core.vi import Button, print_information, print_error
 from .integers import find_integers, correct_angle
 _treat = _("Treat")
+CELL_TYPE_C, CELL_TYPE_N = zip(*(
+    ("hex", _("Hexagonal")),
+))
 
 
 class IFloat(TabCell):
@@ -60,6 +63,33 @@ class X0Cell(TabCell):
         pass
 
 
+class HklCell(TabCell):
+    """Cell with Miller indices"""
+    def __init__(self, indices):
+        self.__value = None
+        self._indices = indices
+        super().__init__()
+
+    @property
+    def value(self):
+        return self._indices.get(self.row)
+
+    @value.setter
+    def value(self, val):
+        try:
+            ls = list(map(int, val.split()[:3]))
+        except (ValueError, AttributeError):
+            return
+        ls += [0] * (3 - len(ls))
+        self._indices[self.row] = self.__value = ls
+
+    def __str__(self):
+        v = self.value
+        if v is None:
+            return ""
+        return "%d %d %d" % tuple(v)
+
+
 class DisplayX0:
     def __init__(self, units, idat):
         self.units = units
@@ -81,6 +111,7 @@ class DisplayX0:
 class FoundBells(Spreadsheet):
     def __init__(self, xrd):
         self._xrd = xrd
+        self._uindex = xrd.extra_data.setdefault("UserIndexes", {})
         cryb = xrd.extra_data.get("crypbells")
         self.cryb = cryb = sorted(map(tuple, cryb.reshape(len(cryb) // 4, 4)))
         val = Tabular(colnames=["x\u2080", "h", "w", "s"])
@@ -89,11 +120,14 @@ class FoundBells(Spreadsheet):
             val.insert_row(i, [X0Cell(data[0], display)] + [
                 IFloat(i) for i in data[1:]])
         super().__init__(str(xrd.name) + _(" (found reflexes)"), val)
+        self.load_miller_indices()
         self.int_groups = []
         self.menu.append_item((_treat,), _("Find integers"),
                               self._find_ints, None)
         self.menu.append_item((_treat,), _("Correct angle"),
                               self._theta_correction, None)
+        self.menu.append_item((_treat,), _("Add user indexes..."),
+                              self.add_user_indexes, None)
         self.show()
         self.set_form([(_("Units to display x\u2080:"), (
             "sin(\u03b8)", "d (\u212b)", "d\u207b\u00b2 (\u212b\u207b\u00b2)",
@@ -140,6 +174,35 @@ class FoundBells(Spreadsheet):
     def select_units(self, u):
         self.display.units = ["sin", "d", "d2", "theta", "2theta"][u]
         self.value.refresh()
+
+    def add_user_indexes(self):
+        dlgr = self.input_dialog(_("New index column"), [
+            (_("Name:"), ""), (_("Cell:"), CELL_TYPE_N)])
+        if dlgr is None:
+            return
+        name, cell = dlgr
+        if name in self._uindex:
+            self.print_error(
+                _("Index with name `%s' already exists") % name)
+            return
+        indices = {}
+        self._uindex[name] = {"cell": CELL_TYPE_C[cell], "indices": indices}
+        self.value.insert_column(self.value.columns, name,
+                                 lambda x=indices: HklCell(x))
+
+    def load_miller_indices(self):
+        for name in self._uindex:
+            try:
+                indices = self._uindex[name]["indices"]
+            except KeyError:
+                continue
+            try:
+                indices = dict((int(k), v) for k, v in indices.items())
+            except ValueError:
+                continue
+            self._uindex[name]["indices"] = indices
+            self.value.insert_column(self.value.columns, name,
+                                     lambda x=indices: HklCell(x))
 
 
 def show_sheet(xrd):
