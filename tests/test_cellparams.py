f56a8b373d54aa01033733918bb7b34fa8cc5b5e
Serhii Lysovenko <Lysovenko@users.noreply.github.com>
2021-05-28 21:47:40 +0300

    Prepare tests for rhomboedral and monoclinic syngonies

--- tests/test_cellparams.py
+++ tests/test_cellparams.py
@@ -2,7 +2,7 @@ import unittest
 from sys import path
 path.append("../components/cryp")
 from itertools import product
-from numpy import array, sqrt, zeros
+from numpy import array, sqrt, zeros, sin, tan, cos
 from cellparams import calc_orhomb, calc_hex, calc_tetra, calc_cubic
 
 
@@ -45,6 +45,35 @@ class TestCellparams(unittest.TestCase):
         d = sqrt(1. / d)
         dhkl = array(list(zip(d, h, k, el))).transpose()
         self.assertEqual(calc_cubic(dhkl)[0], a)
+
+    def test_rhombohedral(self):
+        hkl = array(list(product(*((tuple(range(5)),) * 3)))[1:]).transpose()
+        h, k, el = hkl
+        a, alp = 3., 1.5
+        d = 1. / a ** 2. * (((1. + cos(alp))
+                             * ((h ** 2 + k ** 2 + el ** 2)
+                                - (1. - tan(alp / 2.) ** 2)
+                                * (h * k + k * el + el * h)))
+                            / (1. + cos(alp) - 2. * cos(alp) ** 2))
+        d = sqrt(1. / d)
+        dhkl = zeros((4, len(d)))
+        dhkl[0, :] = d
+        dhkl[1:, :] = hkl
+        # self.assertEqual(calc_rhombohedral(dhkl)[:4], (a, None, None, alp))
+
+    def test_monoclinic(self):
+        hkl = array(list(product(*((tuple(range(5)),) * 3)))[1:]).transpose()
+        h, k, el = hkl
+        a, b, c, bet = 3., 4., 5., 1.5
+        d = (1. / a ** 2. * (h ** 2 / sin(bet) ** 2)
+             + 1. / b ** 2 * k ** 2
+             + 1 / c ** 2 * el ** 2 / (sin(bet) ** 2)
+             - 2 * h * el / (a * c * sin(bet) * tan(bet)))
+        d = sqrt(1. / d)
+        dhkl = zeros((4, len(d)))
+        dhkl[0, :] = d
+        dhkl[1:, :] = hkl
+        # self.assertEqual(calc_monoclinic(dhkl)[:5], (a, b, c, None, bet))
 
 
 if __name__ == "__main__":
