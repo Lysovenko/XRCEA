bdcd3ab7f0c599e94dcb873e84286d1a0ec97316
Serhii Lysovenko <Lysovenko@users.noreply.github.com>
2023-05-30 15:32:52 +0300

    Less boilerplate

--- xrcea/core/description.py
+++ xrcea/core/description.py
@@ -36,9 +36,24 @@ class Description:
     def append(self, val):
         self.document.append(val)
 
+    write = append
+
 
 class DescItem:
-    pass
+    def __init__(self):
+        self.content = []
+
+    def __iter__(self):
+        return self.content.__iter__()
+
+    def __getitem__(self, i):
+        return self.content.__getitem__(i)
+
+    def __len__(self):
+        return self.content.__len__()
+
+    def write(self, data):
+        self.content.append(data)
 
 
 class Title(DescItem):
@@ -49,80 +64,21 @@ class Title(DescItem):
 
 class Paragraph(DescItem):
     def __init__(self, text=None):
-        self.content = []
+        super().__init__()
         if text is not None:
-            self.content.append(text)
-
-    def get_obj(self):
-        par = {type: self.__name__}
-        par["content"] = [i.get_obj() for i in self.content]
-        return par
-
-    def __iter__(self):
-        return self.content.__iter__()
-
-    def __getitem__(self, i):
-        return self.content.__getitem__(i)
-
-    def __len__(self):
-        return self.content.__len__()
+            self.write(text)
 
 
 class Table(DescItem):
-    def __init__(self):
-        self.content = []
-
-    def get_obj(self):
-        par = {type: self.__name__}
-        par["content"] = [i.get_obj() for i in self.content]
-        return par
-
-    def __iter__(self):
-        return self.content.__iter__()
-
-    def __getitem__(self, i):
-        return self.content.__getitem__(i)
-
-    def __len__(self):
-        return self.content.__len__()
+    pass
 
 
 class Row(DescItem):
-    def __init__(self):
-        self.content = []
-
-    def get_obj(self):
-        par = {type: self.__name__}
-        par["content"] = [i.get_obj() for i in self.content]
-        return par
-
-    def __iter__(self):
-        return self.content.__iter__()
-
-    def __getitem__(self, i):
-        return self.content.__getitem__(i)
-
-    def __len__(self):
-        return self.content.__len__()
+    pass
 
 
 class Cell(DescItem):
-    def __init__(self):
-        self.content = []
-
-    def get_obj(self):
-        par = {type: self.__name__}
-        par["content"] = [i.get_obj() for i in self.content]
-        return par
-
-    def __iter__(self):
-        return self.content.__iter__()
-
-    def __getitem__(self, i):
-        return self.content.__getitem__(i)
-
-    def __len__(self):
-        return self.content.__len__()
+    pass
 
 
 class SubScript(DescItem):
