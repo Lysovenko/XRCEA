04e2add6865a59edf8204691daf4c5f83bcd58ab
Serhii Lysovenko <Lysovenko@users.noreply.github.com>
2024-08-30 15:37:26 +0300

    Learn dataclass :-D

--- xrcea/components/cryp/pattern.py
+++ xrcea/components/cryp/pattern.py
@@ -14,5 +14,38 @@
 # along with this program; if not, write to the Free Software Foundation,
 # Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 """
-About patterns
+About adjustable patterns
 """
+from dataclasses import dataclass, field
+from typing import List, Optional
+from enum import Enum
+
+
+class Syngony(Enum):
+    "Syngony of crystalline cell"
+    Triclinic = 1
+    Monoclinic = 2
+    Rhombic = 3
+    Trigonal = 4
+    Tetragonal = 5
+    Hexagonal = 6
+    Cubic = 7
+
+
+@dataclass
+class CellParams:
+    "Parameters of the cell"
+    syngony: Syngony
+    a: float
+    b: Optional[float] = None
+    c: Optional[float] = None
+    alpha: Optional[float] = None
+    beta: Optional[float] = None
+    gamma: Optional[float] = None
+
+
+@dataclass
+class AdjPat:
+    "Adjustable pattern"
+    cell: CellParams
+    reflexes: List = field(default_factory=list)
