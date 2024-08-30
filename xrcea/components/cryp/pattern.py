43d566583838f9322d6b93d46940ac22f3352722
Serhii Lysovenko <Lysovenko@users.noreply.github.com>
2024-08-30 21:15:26 +0300

    calculate some reflexes

--- xrcea/components/cryp/pattern.py
+++ xrcea/components/cryp/pattern.py
@@ -19,17 +19,19 @@ About adjustable patterns
 from dataclasses import dataclass, field
 from typing import List, Optional
 from enum import Enum
+from math import sqrt, tan, cos, sin
 
 
 class Syngony(Enum):
     "Syngony of crystalline cell"
-    Triclinic = 1
-    Monoclinic = 2
-    Rhombic = 3
-    Trigonal = 4
-    Tetragonal = 5
-    Hexagonal = 6
-    Cubic = 7
+    Triclinic = 6
+    Monoclinic = 5
+    Rhombic = 4
+    Trigonal = 3
+    Rhombohedral = 3
+    Tetragonal = 2
+    Hexagonal = 1
+    Cubic = 0
 
 
 @dataclass
@@ -42,6 +44,76 @@ class CellParams:
     alpha: Optional[float] = None
     beta: Optional[float] = None
     gamma: Optional[float] = None
+
+    def _d_cubic(self, hkl):
+        """Calculate d_hkl for Cubic unit cell"""
+        h, k, el = hkl
+        d2 = 1.0 / self.a**2 * (h**2 + k**2 + el**2)
+        return sqrt(1.0 / d2)
+
+    def _d_orhomb(self, hkl):
+        """Calculate d_hkl for Orthorhombic unit cell"""
+        h, k, el = hkl
+        d2 = (
+            1.0 / self.a**2 * h**2
+            + 1.0 / self.b**2 * k**2
+            + 1.0 / self.c**2 * el**2
+        )
+        return sqrt(1.0 / d2)
+
+    def _d_hex(self, hkl):
+        """Calculate d_hkl for Hexagonal unit cell"""
+        h, k, el = hkl
+        d2 = (
+            4.0 / 3.0 / self.a**2 * (h**2 + h * k + k**2)
+            + 1.0 / self.c**2 * el**2
+        )
+        return sqrt(1.0 / d2)
+
+    def _d_tetra(self, hkl):
+        """Calculate d_hkl for Tetrahonal unit cell"""
+        h, k, el = hkl
+        d2 = (
+            1.0 / self.a**2 * (h**2 + k**2) + 1.0 / self.c**2 * el**2
+        )
+        return sqrt(1.0 / d2)
+
+    def _d_rhombohedral(self, hkl):
+        """Calculate d_hkl for Rhombohedral unit cell"""
+        h, k, el = hkl
+        d2 = (
+            1.0
+            / self.a**2.0
+            * (1.0 + cos(self.alpha))
+            * (
+                (h**2 + k**2 + el**2)
+                - (1.0 - tan(self.alpha / 2.0) ** 2)
+                * (h * k + k * el + el * h)
+            )
+            / (1.0 + cos(self.alpha) - 2.0 * cos(self.alpha) ** 2)
+        )
+        return sqrt(1.0 / d2)
+
+    def _d_monoclinic(self, hkl):
+        """Calculate d_hkl for Monoclinic unit cell"""
+        h, k, el = hkl
+        d2 = (
+            1.0 / self.a**2.0 * (h**2 / sin(self.beta) ** 2)
+            + 1.0 / self.b**2 * k**2
+            + 1 / self.c**2 * el**2 / (sin(self.bet) ** 2)
+            - 2 * h * el / (self.a * self.c * sin(self.beta) * tan(self.beta))
+        )
+        return sqrt(1.0 / d2)
+
+    def reflex(self, hkl):
+        return [
+            self._d_cubic,
+            self._d_hex,
+            self._d_tetra,
+            self._d_rhombohedral,
+            self._d_orhomb,
+            self._d_monoclinic,
+        ][self.syngony.value](hkl)
 
 
 @dataclass
