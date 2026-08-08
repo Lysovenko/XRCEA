b8ba6df5910441bc6b8d7a6a925ac31820ebafe6
Serhii Lysovenko <Lysovenko@users.noreply.github.com>
2026-08-08 15:05:21 +0300

    Fix a bug in menu creation

--- xrcea/core/vi/menu.py
+++ xrcea/core/vi/menu.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Menu abstraction"""
-# XRCEA (C) 2019 Serhii Lysovenko
+# XRCEA (C) 2019-2026 Serhii Lysovenko
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -29,6 +29,7 @@ class DMenuItem:
 
 class DMenu:
     """Dynamic menu"""
+
     def __init__(self):
         self.root = {}
 
@@ -40,12 +41,16 @@ class DMenu:
         t_con = self.get_container(path, {})
         isec = set(t_con).intersection(o_con)
         uniq = set(t_con).symmetric_difference(o_con)
-        rlist = [((t_con[i].priority + o_con[i].priority) / 2, i, t_con[i])
-                 for i in isec]
-        rlist += [(t_con[i].priority, i, t_con[i])
-                  for i in uniq.intersection(t_con)]
-        rlist += [(o_con[i].priority, i, o_con[i])
-                  for i in uniq.intersection(o_con)]
+        rlist = [
+            ((t_con[i].priority + o_con[i].priority) / 2, i, t_con[i])
+            for i in isec
+        ]
+        rlist += [
+            (t_con[i].priority, i, t_con[i]) for i in uniq.intersection(t_con)
+        ]
+        rlist += [
+            (o_con[i].priority, i, o_con[i]) for i in uniq.intersection(o_con)
+        ]
         rlist.sort()
         return [i[1:] for i in rlist]
 
@@ -57,66 +62,91 @@ class DMenu:
             except KeyError:
                 if default is None:
                     rcont = {}
-                    self.append_item(path[:i - 1], path[i], rcont)
+                    self.append_item(path[:i], path[i], rcont)
                 else:
                     return default
         return rcont
 
-    def insert_item(self, path, priority, name, function, shortcut,
-                    description=None, icon=None):
+    def insert_item(
+        self,
+        path,
+        priority,
+        name,
+        function,
+        shortcut,
+        description=None,
+        icon=None,
+    ):
         """inserts an item to the menu"""
         if isinstance(path, dict):
             cont = path
         else:
             cont = self.get_container(path)
         if name in cont:
-            if isinstance(cont[name].function, dict) and \
-               isinstance(function, dict):
+            if isinstance(cont[name].function, dict) and isinstance(
+                function, dict
+            ):
                 return
-            raise RuntimeError("only submenu can be registred twice %s/%s"
-                               % ("/".join(path), name))
+            raise RuntimeError(
+                "only submenu can be registred twice %s/%s"
+                % ("/".join(path), name)
+            )
         cont[name] = mi = DMenuItem(priority, name, function)
         mi.shortcut = shortcut
         mi.description = description
         mi.icon = icon
 
-    def append_item(self, path, name, function, shortcut=None,
-                    description=None, icon=None):
+    def append_item(
+        self, path, name, function, shortcut=None, description=None, icon=None
+    ):
         """appends an item to the menu"""
         cont = self.get_container(path)
         if cont:
             priority = max(val.priority for val in cont.values()) + 1
         else:
             priority = 1
-        self.insert_item(cont, priority, name, function, shortcut,
-                         description, icon)
+        self.insert_item(
+            cont, priority, name, function, shortcut, description, icon
+        )
 
-    def insert_item_after(self, path, name, function,
-                          shortcut, description=None, icon=None):
+    def insert_item_after(
+        self, path, name, function, shortcut, description=None, icon=None
+    ):
         """inserts an item to the menu after the appointed item"""
         cont = self.get_container(path[:-1])
         priority = cont[path[-1]].priority
         gt = [val.priority for val in cont.values() if val.priority > priority]
         if gt:
-            priority = (priority + min(gt)) / 2.
+            priority = (priority + min(gt)) / 2.0
         else:
             priority += 1
-        self.insert_item(cont, priority, name, function, shortcut,
-                         description, icon)
+        self.insert_item(
+            cont, priority, name, function, shortcut, description, icon
+        )
 
-    def insert_item_before(self, path, name, title, function,
-                           shortcut, description=None, icon=None,
-                           visibility="", action_catch=None):
+    def insert_item_before(
+        self,
+        path,
+        name,
+        title,
+        function,
+        shortcut,
+        description=None,
+        icon=None,
+        visibility="",
+        action_catch=None,
+    ):
         """inserts an item to the menu before the appointed item"""
         cont = self.get_container(path[:-1])
         priority = cont[path[-1]].priority
         lt = [val.priority for val in cont.values() if val.priority < priority]
         if lt:
-            priority = (priority + max(lt)) / 2.
+            priority = (priority + max(lt)) / 2.0
         else:
             priority -= 1
-        self.insert_item(cont, priority, name, function, shortcut,
-                         description, icon)
+        self.insert_item(
+            cont, priority, name, function, shortcut, description, icon
+        )
 
     def remove_item(self, path):
         """removes an item from the menu"""
